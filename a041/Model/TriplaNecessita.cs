eed607347cf4e3bed15448f2e368783c628892de
duofresh <ccoccimiglio5@gmail.com>
2024-09-26 09:08:32 +0200

    a

--- a041/Model/TriplaNecessita.cs
+++ a041/Model/TriplaNecessita.cs
@@ -15,17 +15,12 @@ namespace a041.Model
         {
             StringBuilder result = new StringBuilder();
 
-            // Loop through each class
             foreach (var classe in gestioneOrario.Classi)
             {
                 result.AppendLine($"Classe: {classe.ToString()}");
-
-                // Loop through each subject (Disciplina)
                 foreach (var disciplina in gestioneOrario.Discipline)
                 {
-                    // Here you would need to establish the connection between classes and subjects
-                    // For now, I am assuming that the subject is part of the class name (as inferred from previous logic)
-                    if (classe.ToString().Contains(disciplina.trovaDisciplina(disciplina.Codice))) // Example logic
+                    if (classe.ToString().Contains(disciplina.trovaDisciplina(disciplina.Codice)))
                     {
                         result.AppendLine($"  Disciplina: {disciplina.trovaDisciplina(disciplina.Codice)} - Ore: {classe.GetOreTotali()}");
                     }
